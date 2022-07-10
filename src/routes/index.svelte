270cd9f361cdfc0dabad9298e2b0c9754484933f
Jacob Nielsen <jnillern@gmail.com>
2022-07-10 20:21:55 +0200

    change frontpage

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,3 @@
 <div class="flex h-[100vh] flex-col items-center justify-center">
-	<h1>Welcome to SvelteKit</h1>
-	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+	<h1>Work in progress</h1>
 </div>
